563463275563b3fd99f2b293b1629e1a11394be2
SY <48312123+minakusi@users.noreply.github.com>
2024-04-19 21:51:22 +0900

    Title: 블럭쌓는 명령, Time: 1012ms, Memory: 4MB, Status: Time Limit Exceed - CodeTree

--- 240419/블럭쌓는 명령/block-stacking-commands.cpp
+++ 240419/블럭쌓는 명령/block-stacking-commands.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
     int n, k;
     cin >> n >> k;
     vector<int> block(n+1, 0);
