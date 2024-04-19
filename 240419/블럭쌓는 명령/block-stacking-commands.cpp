996cddf477ae5d2b2ee6a7efdf38c8a1f37f23aa
SY <48312123+minakusi@users.noreply.github.com>
2024-04-19 21:58:04 +0900

    Title: 블럭쌓는 명령, Time: 57ms, Memory: 4MB, Status: Passed - CodeTree

--- 240419/블럭쌓는 명령/block-stacking-commands.cpp
+++ 240419/블럭쌓는 명령/block-stacking-commands.cpp
@@ -7,7 +7,7 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    
+
     int n, k;
     cin >> n >> k;
     vector<int> block(n+1, 0);
@@ -15,8 +15,11 @@ int main() {
     for(int i = 0; i < k; ++i){
         int a, b;
         cin >> a >> b;
-        for (int j = a; j <= b; ++j) block[j]++;
+        ++block[a];
+        --block[b+1];
     }
+
+    for(int i = 0; i < n; ++i) block[i+1] += block[i];
 
     sort(block.begin(), block.end());
 
